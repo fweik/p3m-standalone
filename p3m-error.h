61b13d548cdd906e4918184753a8c9361a0f66a9
Florian Weik <fweik@icq.uni-stuttgart.de>
2011-08-03 16:07:43 +0200

    added first half of error functions.

---
+++ p3m-error.h
@@ -0,0 +1,10 @@
+#ifndef P3M_ERROR_H
+#define P3M_ERROR_H
+
+#define P3M_BRILLOUIN_TUNING 0
+#define PI 3.14159265358979323846264
+
+double p3m_error_ik(double, double, int, double, double, double *, int *, int);
+double p3m_error_add();
+
+#endif
