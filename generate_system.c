bcc48d21a699056ae9382a9981ad3249dbb40ba8
Florian Weik <f.weik@web.de>
2011-08-30 15:19:32 +0200

    Added system generators for madelung and inner box systems.

--- generate_system.c
+++ generate_system.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "generate_system.h"
 
 #include "common.h"
@@ -5,6 +7,46 @@
 #include "math.h"
 
 #include "stdlib.h"
+
+system_t *generate_medelung(int size, FLOAT_TYPE box) {
+  int i,j;
+  system_t *s;
+
+  FLOAT_TYPE a = box/size;
+
+  s = Init_system(size);
+  s->length = box;
+  s->q2 = 0.0;
+
+  for(i=0;i<size;i++) {
+    for(j=0;j<2;j++) {
+      s->p->fields[j][i] = a*i;
+    }
+    s->q[i] = 1.0 - 2.0 * (i%2);
+    s->q2 += s->q[i] * s->q[i];
+  }
+  return s;
+}
+
+system_t *generate_inner_box(int size, FLOAT_TYPE box) {
+  FLOAT_TYPE lower_left = 0.25 * box;
+
+  int i,j;
+  system_t *s;
+
+  s = Init_system(size);
+  s->length = box;
+  s->q2 = 0.0;
+
+  for(i=0;i<size;i++) {
+    for(j=0;j<2;j++) {
+      s->p->fields[j][i] = lower_left + 0.5 * box*drand48();
+    }
+    s->q[i] = 1.0 - 2.0 * (i%2);
+    s->q2 += s->q[i] * s->q[i];
+  }
+  return s;
+}
 
 system_t *generate_random_system(int size, FLOAT_TYPE box, FLOAT_TYPE max_charge ) {
   int i,j;
@@ -25,8 +67,18 @@ system_t *generate_random_system(int size, FLOAT_TYPE box, FLOAT_TYPE max_charge
 }
 
 system_t *generate_system( int form_factor, int size, FLOAT_TYPE box, FLOAT_TYPE max_charge ) {
-  if( form_factor == FORM_FACTOR_RANDOM ) {
+  switch(form_factor) {
+  case FORM_FACTOR_RANDOM: 
     return generate_random_system( size, box, max_charge );
+    break;
+  case FORM_FACTOR_INNER_BOX:
+    return generate_inner_box(size, box);
+    break;
+  case FORM_FACTOR_MADELUNG:
+    return generate_madelung(size,box);
+    break;
+  default:
+    fprtintf( stderr, "Warning, form factor not known.\n");
   }
-  return NULL;
+  return (void *)NULL;
 }
--- generate_system.h
+++ generate_system.h
@@ -3,7 +3,10 @@
 
 #include "common.h"
 
-enum { FORM_FACTOR_RANDOM = 0 };
+enum { FORM_FACTOR_RANDOM = 0,
+       FORM_FACTOR_INNER_BOX = 1,
+       FORM_FACTOR_MADELUNG = 2
+     };
 
 system_t *generate_system( int, int, FLOAT_TYPE, FLOAT_TYPE);
 
