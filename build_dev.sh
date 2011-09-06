167afff0a8242db53bea9c4cd4b532f8c0655c4f
Florian Weik <fweik@icp.uni-stuttgart.de>
2011-09-06 09:07:22 +0200

    provisional build scripts.

---
+++ build_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+mpicc -O0 -g -Wall -DNDEBUG -lm -lfftw3 main.c ewald.c error.c realpart.c common.c io.c p3m-common.c charge-assign.c p3m-ik.c timings.c p3m-ik-i.c greens.c p3m-ad.c p3m-ad-i.c
+
+
+
+
+
