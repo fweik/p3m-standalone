e1a1449ac0a701b5bdeff570240df3d29ac8c34a
Florian Weik <fweik@icp.uni-stuttgart.de>
2014-02-10 10:43:02 +0100

    Added Q-table for ik.

---
+++ q_ik_double.h
@@ -0,0 +1,11 @@
+#ifndef _Q_H
+#define _Q_H
+
+extern const double Q[][7][101];
+extern const int meshes[];
+extern const int n_meshes;
+extern const int n_caos;
+extern const int points;
+extern const double alphaLmax;
+
+#endif
